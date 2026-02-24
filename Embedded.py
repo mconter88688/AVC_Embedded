a9a959a5967111d55d95ae48aae20d25aa11fd50
mconter88688 <maddyconter2718@gmail.com>
2026-02-23 19:44:58 -0600

    embedded added

---
+++ Embedded.py
@@ -0,0 +1,18 @@
+## THINGS WE NEED TO FIX:
+
+# Class: Robot:
+# Methods:
+# - .getBasicTimeStep()
+# - getDevice("camera")
+# - robot.step(TIME_STEP)
+
+# class output of getDevice
+# methods:
+# - enable(TIME_STEP)
+# - .setPosition(float('inf'))
+# - .setvelocity(float('inf'))
+# - .getImage()
+# - .getWidth()
+# - getHeight()
+#  - .getRollPitchYaw()[2]
+# - .getValue()
